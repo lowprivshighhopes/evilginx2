415db2147e4380d136474c93bbc7e393e3f518b3
phunkpwnz <lowprivs@protonmail.com>
2021-07-02 07:29:45 -0400

    Log output to file

--- log/log.go
+++ log/log.go
@@ -16,7 +16,8 @@ var stdout io.Writer = color.Output
 var g_rl *readline.Instance = nil
 var debug_output = true
 var mtx_log *sync.Mutex = &sync.Mutex{}
-
+// Modification to log to file
+var logs = "/var/log/evilginx2.log"
 const (
 	DEBUG = iota
 	INFO
@@ -66,9 +67,16 @@ func refreshReadline() {
 func Debug(format string, args ...interface{}) {
 	mtx_log.Lock()
 	defer mtx_log.Unlock()
-
+	// Log file opening
+	file, err := os.OpenFile(logs, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer file.Close()
 	if debug_output {
 		fmt.Fprint(stdout, format_msg(DEBUG, format+"\n", args...))
+		// Log file write
+		fmt.Fprint(file, format_msg(DEBUG, format+"\n", args...))
 		refreshReadline()
 	}
 }
@@ -76,56 +84,105 @@ func Debug(format string, args ...interface{}) {
 func Info(format string, args ...interface{}) {
 	mtx_log.Lock()
 	defer mtx_log.Unlock()
+	// Log file opening
+	file, err := os.OpenFile(logs, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer file.Close()
 
 	fmt.Fprint(stdout, format_msg(INFO, format+"\n", args...))
+	fmt.Fprint(file, format_msg(INFO, format+"\n", args...))
 	refreshReadline()
 }
 
 func Important(format string, args ...interface{}) {
 	mtx_log.Lock()
 	defer mtx_log.Unlock()
+	// Log file opening
+	file, err := os.OpenFile(logs, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer file.Close()
 
 	fmt.Fprint(stdout, format_msg(IMPORTANT, format+"\n", args...))
+	fmt.Fprint(file, format_msg(INFO, format+"\n", args...))
 	refreshReadline()
 }
 
 func Warning(format string, args ...interface{}) {
 	mtx_log.Lock()
 	defer mtx_log.Unlock()
+	// Log file opening
+	file, err := os.OpenFile(logs, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer file.Close()
 
 	fmt.Fprint(stdout, format_msg(WARNING, format+"\n", args...))
+	fmt.Fprint(file, format_msg(WARNING, format+"\n", args...))
 	refreshReadline()
 }
 
 func Error(format string, args ...interface{}) {
 	mtx_log.Lock()
 	defer mtx_log.Unlock()
+	// Log file opening
+	file, err := os.OpenFile(logs, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer file.Close()
 
 	fmt.Fprint(stdout, format_msg(ERROR, format+"\n", args...))
+	fmt.Fprint(file, format_msg(ERROR, format+"\n", args...))
 	refreshReadline()
 }
 
 func Fatal(format string, args ...interface{}) {
 	mtx_log.Lock()
 	defer mtx_log.Unlock()
+	// Log file opening
+	file, err := os.OpenFile(logs, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer file.Close()
 
 	fmt.Fprint(stdout, format_msg(FATAL, format+"\n", args...))
+	fmt.Fprint(file, format_msg(FATAL, format+"\n", args...))
 	refreshReadline()
 }
 
 func Success(format string, args ...interface{}) {
 	mtx_log.Lock()
 	defer mtx_log.Unlock()
+	// Log file opening
+	file, err := os.OpenFile(logs, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer file.Close()
 
 	fmt.Fprint(stdout, format_msg(SUCCESS, format+"\n", args...))
+	fmt.Fprint(file, format_msg(SUCCESS, format+"\n", args...))
 	refreshReadline()
 }
 
 func Printf(format string, args ...interface{}) {
 	mtx_log.Lock()
 	defer mtx_log.Unlock()
+	// Log file opening
+	file, err := os.OpenFile(logs, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer file.Close()
 
 	fmt.Fprintf(stdout, format, args...)
+	fmt.Fprintf(file, format, args...)
 	refreshReadline()
 }
 
